9779e076baa0cf13099fd14d9f4de5a63f346823
Ashley King <ashleyot@gmail.com>
2019-05-25 17:23:57 -0400

    created icons object and links object

---
+++ src/constants/links.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    path: "/",
+    text: "home",
+  },
+  {
+    path: "/tours",
+    text: "tours",
+  },
+  {
+    path: "/contact",
+    text: "contact",
+  },
+  {
+    path: "/blog",
+    text: "blog",
+  },
+  {
+    path: "/error",
+    text: "error",
+  },
+  {
+    path: "/rooms",
+    text: "rooms",
+  }
+]

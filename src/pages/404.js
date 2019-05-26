e30d32fc1d12e1c9aa85001331bd9df43876d7db
Ashley King <ashleyot@gmail.com>
2019-05-26 09:29:03 -0400

    finished 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,21 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../css/error.module.css"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
+
 const error = () => {
-  return <Layout>You've reached a dead end.</Layout>
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="deadend dude. let's head back.">
+          <Link to="/" className="btn-white">
+            Back Home
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
 
 export default error

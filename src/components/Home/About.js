6d11074a581f844cd20d3af88393a9c8d721eea5
Ashley King <ashleyot@gmail.com>
2019-05-26 10:31:03 -0400

    about section done

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,11 +1,34 @@
 import React from "react"
 import Title from "../Title"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
+
 const About = () => {
   return (
-    <div>
-      <h1>Hello from about</h1>
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <img src={img} alt="about company" />
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>
+           
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            provident est esse.
+          </p>
+          <p>
+        
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+            provident est esse.
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
